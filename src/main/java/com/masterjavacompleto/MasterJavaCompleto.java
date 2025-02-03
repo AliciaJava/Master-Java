bc9893df993dacfeb6b09e64ec802063e2771ddf
AliciaJava <Aliciaortega1986@gmail.com>
2025-02-03 12:02:24 +0100

    Ejemplo Master Java Completo

---
+++ src/main/java/com/masterjavacompleto/MasterJavaCompleto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.masterjavacompleto;
+
+/**
+ *
+ * @author 2023
+ */
+public class MasterJavaCompleto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
